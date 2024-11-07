d4bd9611579cded011bfd6d6e1947b69ff528334
shyian-code <shyian.code@gmail.com>
2024-11-07 22:53:43 +0200

    init project

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,77 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import vituum from 'vituum';
+import posthtml from '@vituum/vite-plugin-posthtml';
+import postcss from '@vituum/vite-plugin-postcss';
+import { copyFileSync } from 'fs';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default {
+  plugins: [
+    vituum(),
+    postcss(),
+    posthtml({
+      root: './src',
+    }),
+
+    {
+      name: 'custom-hmr',
+      enforce: 'post',
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith('.html')) {
+          server.ws.send({
+            type: 'full-reload',
+            path: '*',
+          });
+        }
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+    }, 
+    {
+      name: 'copy-static-files',
+      writeBundle() {
+        copyFileSync('src/android-chrome-192x192.png', 'dist/android-chrome-192x192.png');
+        copyFileSync('src/android-chrome-512x512.png', 'dist/android-chrome-512x512.png');
+      },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
-});
+  ],
+
+  build: {
+    root: './src',
+    rollupOptions: {
+      output: {
+        assetFileNames: (asset) => {
+          const filePath = asset.name.split('/');
+          const fileName = filePath.pop();
+          const nestedPath = filePath.join('/');
+          const outputPath = `${
+            nestedPath ? nestedPath + '/' : ''
+          }[name][extname]`;
+
+          if (asset.name.includes('favicon') || asset.name.includes('apple-touch-icon') || asset.name.includes('android-chrome') ) {
+            return `${outputPath}`;
+          }
+
+          console.log(`${asset} - ${asset.name} - ${asset.type}`);
+            console.dir(`${asset}`);
+
+          if (asset.type === 'asset') {
+            switch (asset.name.split('.').pop()) {
+              case 'css':
+                return `css/${outputPath}`;
+              case 'png':
+              case 'jpg':
+              case 'webp':
+              case 'svg':
+                return `images/${outputPath}`;
+              case 'woff2':
+                return `fonts/${outputPath}`;
+              case 'webmanifest':
+                return `${outputPath}`;
+              default:
+                return `other/${outputPath}`;
+            }
+          }
+        },
+        preserveModuleDirectories: true,
+      }
+    },
+  },
+};
+
